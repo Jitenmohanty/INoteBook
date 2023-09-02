b5d68da929fbc5965c3ce197db746bf82ba6eb4f
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-09-02 21:06:27 +0530

    Lol

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
-
+import {BrowserRouter as Router,Routes ,Route} from 'react-router-dom';
+import Home from './components/Home'
+import Navbar from './components/Navbar';
+import About from './components/About';
 function App() {
   return (
-    <div className="App">
-    Lol
-    </div>
+<Router>
+  <Navbar/>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<About/>}/>
+  </Routes>
+</Router>
+      
   );
 }
 
