2ad61597cab5558f2706f707bddb3464acff6108
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-09-02 11:48:55 +0530

    Update Notes Route done

--- Backend/Controller/noteController.js
+++ Backend/Controller/noteController.js
@@ -13,12 +13,33 @@ module.exports.addNotes = async (req, res, next) => {
     try {
         const { title, description, tag } = req.body;
         const note = new Notes({
-            title,description,tag,user:req.user.id
+            title, description, tag, user: req.user.id
         })
         const saveNote = await note.save();
         return res.json({ status: true, saveNote });
     } catch (ex) {
-        return res.json({msg:"Error on add Note",status:false})
+        return res.json({ msg: "Error on add Note", status: false })
         next(ex);
+    }
+}
+
+module.exports.updateNotes = async (req, res, next) => {
+    try {
+        const { title, description, tag } = req.body;
+        const newNote = {};
+        if (title) { newNote.title = title }
+        if (description) { newNote.description = description }
+        if (tag) { newNote.tag = tag }
+
+        let note = await Notes.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not found") };
+
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed")
+        }
+        note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+        res.json({ note })
+    } catch (error) {
+        return res.json({msg:"Error on update notes",status:false})
     }
 }
--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const fetchuser = require('../Middleware/userCredential');
-const { getAllNotes, addNotes } = require('../Controller/noteController')
+const { getAllNotes, addNotes, updateNotes } = require('../Controller/noteController')
 
 
 router.get('/getallnotes',fetchuser,getAllNotes);
 router.post('/addnote',fetchuser,addNotes);
+router.put('/updatenote/:id',fetchuser,updateNotes);
 
 module.exports = router;
