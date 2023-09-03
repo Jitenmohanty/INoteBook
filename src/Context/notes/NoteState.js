94239089a0ed32b54fc918e5e988036cd499c062
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-09-03 20:00:18 +0530

    Use contest passing value also fuction Using object

--- src/Context/notes/NoteState.js
+++ src/Context/notes/NoteState.js
@@ -1,12 +1,23 @@
+import { useState } from 'react'
 import NoteContext from './notesContext'
 const NoteState = ({children}) => {
-    const state= {
+    const s1= {
         "name": "Jitu",
         "class": "9A",
     }
 
+    const [state,setState] = useState(s1);
+    const update = ()=>{
+        setTimeout(()=>{
+            setState({
+                "name": "Kitu",
+                "class": "15C",
+            })
+        },2000)
+    }
+
     return (
-        <NoteContext.Provider value={state}>
+        <NoteContext.Provider value={{state,update}}>
             {children}
         </NoteContext.Provider>
     )
--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,16 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import NoteContext from '../Context/notes/notesContext';
 
 const About = () => {
   const a = useContext(NoteContext);
+  useEffect(() => {
+    a.update();
+  }, [])
   return (
     <div>
-      About : my name is {a.name} and class {a.class}
+      About : my name is {a.state.name} and class {a.state.class}
     </div>
   )
 }
 
-export default About
+export default About;
